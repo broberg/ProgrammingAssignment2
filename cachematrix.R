755faa36621a6b1a32d984d68012ab57db25e192
broberg <morten.broberg@gmail.com>
2014-07-20 16:26:05 +0200

    initial commit for assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix will create an object to store two matrices along with function to manipulate these.
+## cacheSolve will be able to use an object create by makeCacheMatrix to calculate the inverse matrix, and
+## then store it in the original object.
 
-## Write a short comment describing this function
+## Creates an object to hold two vectors with getters and setters.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    im <- NULL 
+    
+    set <- function(y){
+        x <<- y # Set internal matrix object
+        im <<- NULL # Set internal inverse matrix object to null
+    }
+    get <- function(){
+        x # Return the internal matrix object
+    }
+    getInverse <- function(){
+        im # Return the cached inverse matrix (or NULL if not calculated) 
+    }
+    setInverse <- function(inv = matrix()){ # Check for a matrix variable
+        im <<- inv # Set the internal matrix object to the matrix var.
+    }
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will take an object create by makeCacheMatrix and either retrieve or calculate and
+## set the objects internal inverse matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        im <- x$getInverse() # Get the Inverse matrix, or NULL if not set
+        
+        if (!is.null(im)){ # Check for non-null value
+            message ("Getting cached data") # Play nice and send a diagnostic message
+            return (im) # Return the allready calculated inverse, use return to exit the function
+        }
+        data <- x$get() # Inverse was not calculated, get the matrix
+        im <- solve(data, ...)  # Use solve to calculate the Inverse matrix, and also use
+                                # triple dots to pass on any extra vars from function call
+        x$setInverse(im) # Set the cahce object to the fresly calculated
+        im # And also return it 
 }
